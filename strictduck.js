cef748b9044e905693cae2e77c3b8cf54bf2191d
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2016-03-05 01:20:52 -0600

    extension

--- strictduck.js
+++ strictduck.js
@@ -1,6 +1,31 @@
+import Duckface from 'Duckface/src/duckface'
+
+export function shouldImplement({
+    name = 'strictduckInterface', methods = []
+}){
+    let face = new Duckface(name, methods);
+    return instance => Duckface.ensureImplements(instance, face)
+}
+
 export default class StrictDuck {
-    constructor(instance, ...Interfaces=[]){
+    constructor(instance, ...interfaces=[]){
         Object.assign(this, instance)
-        Interfaces.map(i => i(this))
+        interfaces.forEach(
+            i => typeof(i == 'function') ?
+                i(this) :
+                shouldImplement(i)(this)
+        )
     }
+}
+
+export function extension({
+    name, parent=StrictDuck, interfaces=[]
+}){
+    let classDict = {}
+    classDict[name] = class extends parent {
+        constructor(instance, ...otherInterfaces=[]){
+            super(instance, ...interfaces=[], ...otherInterfaces)
+        }
+    }
+    return classDict[name]
 }
